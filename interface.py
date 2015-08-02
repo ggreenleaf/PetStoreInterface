80bbda8890ef7dd6774f4c1539e94642f3419d1b
Geoffrey Greenleaf <ggreenleaf@geoffreys-mbp.attlocal.net>
2015-08-02 15:35:42 -0500

    can update values of tuples

--- interface.py
+++ interface.py
@@ -12,16 +12,13 @@ tables = (("PET_STORE",("sid","StreetNo","StreetName","City","State","phone_numb
 				("STOCKS",("sid","pid","quantity")),
 				("SELLS",("tid","pid","t_quantity")))
 
-def display_tables():
-	for i, table in enumerate(tables):
-		print i,table[0]
 
-def get_table_choice():
-	display_tables()
-	choice = raw_input("Please select valid table number: ")
+def get_choice(upper_bound):
+	'''will return a valid integer choice between [0,upper_bound)'''
+	choice = raw_input("Please enter a valid number: ")
 	while True:
 		try:
-			if int(choice) < 0 or int(choice) > len(tables) - 1:
+			if int(choice) < 0 or int(choice) > upper_bound:
 				print "please select a valid table number. You entered ",int(choice)
 				choice = raw_input("Please enter a valid number: ")
 			else:
@@ -30,13 +27,46 @@ def get_table_choice():
 			print "please enter a number.You entered %s."%choice
 			choice = raw_input("Please enter a number: ")
 
-		
 	return int(choice)
 
-def update(cursor):
-	pass
+def display_tables():
+	for i, table in enumerate(tables):
+		print i,table[0]
 
 
+def get_table_choice():
+	display_tables()
+
+	choice = get_choice(len(tables))
+	return choice
+
+def modify(cursor):
+	#Modify alg
+	#display tables/select
+	#select valid entry to update
+	#input new value
+	#create sql
+	#execute sql
+
+	choice = get_table_choice()
+	query = "Select * from %s" % tables[choice][0]
+	cursor.execute(query)
+	print "  ","\t".join(i for i in tables[choice][1])
+	saved_data = []
+	for i,t in enumerate(cursor):
+			print "%d)"%i, "\t".join(str(att) for att in t)
+			saved_data.append(t)
+
+	tuple_choice = get_choice(len(saved_data))
+	print "updating entry:\n\t%s\n Please enter the new values you wish to change"%(" ".join(str(val) for val in saved_data[tuple_choice]))
+	print "To keep the same value reenter the same value"
+	update_cond = tuple(raw_input("%s: "%attribute) for attribute in tables[choice][1])
+	update_sql = (
+			"UPDATE %s SET %s WHERE (%s)")%(tables[choice][0],
+				", ".join("%s='%s'"%(col,val) for col,val in zip(tables[choice][1],update_cond)),
+				" AND ".join("%s='%s'"%(col,val) for col,val in zip(tables[choice][1],saved_data[tuple_choice])))
+	cursor.execute(update_sql)
+	
 
 def delete(cursor):
 	choice = get_table_choice()
@@ -47,12 +77,12 @@ def delete(cursor):
 	for i,t in enumerate(cursor):
 			print "%d)"%i, "\t".join(str(att) for att in t)
 			saved_data.append(t)
-
-	delete_cond = tuple("%s='%s'"%(col,val) for col,val in zip(tables[choice][1],saved_data[6]))
+	print i
+	tuple_choice = get_choice(i) 
+	delete_cond = tuple("%s='%s'"%(col,val) for col,val in zip(tables[choice][1],saved_data[tuple_choice]))
 	delete_sql = (
 		"DELETE FROM %s "
 		"WHERE (%s)")%(tables[choice][0],"AND ".join(i for i in delete_cond))
-	print delete_sql
 	cursor.execute(delete_sql)
 
 
@@ -71,18 +101,17 @@ def insert(cursor):
 	choice = get_table_choice()
 
 	#gathering values for attributes.
-	print "Inserting into table %s" %tables[int(choice)][0]
+	print "Inserting into table %s" %tables[choice][0]
 	print "please enter the data for the attribute listed"
-	data_tuple = tuple([raw_input("%s: "%attribute) for attribute in tables[int(choice)][1]])
-	att_length = len(tables[int(choice)][1])
+	data_tuple = tuple(raw_input("%s: "%attribute) for attribute in tables[choice][1])
+	att_length = len(tables[choice][1])
 	insert_sql = (
 			"INSERT INTO %s (%s) "
-			"Values (%s)" )%(tables[int(choice)][0] , 
-							",".join(i for i in tables[int(choice)][1]),
+			"Values (%s)" )%(tables[choice][0] , 
+							",".join(i for i in tables[choice][1]),
 							",".join(["%s" for i in xrange(att_length)]))
 	
 	cursor.execute(insert_sql,data_tuple)
-
 
 
 def select(cursor):
@@ -102,9 +131,6 @@ def select(cursor):
 	for i,t in enumerate(cursor):
 			print "%d)"%i, "\t".join(str(att) for att in t)
 	
-
-
-
 
 def display_prompt():
 	print """Welcome to the interface to petstore 
@@ -134,7 +160,7 @@ while True:
 		delete(cur)
 
 	elif choice == 3:
-		pass
+		modify(cur)
 
 	elif choice == 4:
 		select(cur)
